bc2eb361746fdb571648570d360ade259ebe471e
Gabriel Trindade <gfktrin@yahoo.com.br>
2018-09-19 13:43:09 -0300

    order in format json returned

--- jarvis/remoteControl/urls.py
+++ jarvis/remoteControl/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<machine>/order', views.order_list, name='order_list'),
+    path('<machine>/order', views.order_return, name='order_return'),
 ]
--- jarvis/remoteControl/views.py
+++ jarvis/remoteControl/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.http import JsonResponse
+
 from .models import Order
 
 # Create your views here.
-def order_list(request,machine):
+def order_return(request,machine):
     order = Order.objects.filter(machineId=machine).latest('createdAt')
-    return render(request, 'remoteControl/order_list.html', {'order': order})
+    jsonResponse = {
+        'orderId' : str(order.id),
+        'order' : order.command,
+        'machine' : str(order.machineId),
+        'createdAt' : str(order.createdAt),
+        'executed' : str(order.executed),
+        'silentMode' : str(order.silentMode),
+    }
+    return JsonResponse(jsonResponse)
